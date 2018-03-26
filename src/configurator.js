76e5b2ad228092ca2263974899e1f53063acfaad
Keith Peters <kp@bit-101.com>
2018-03-26 17:08:30 -0400

    start

--- src/configurator.js
+++ src/configurator.js
@@ -1,14 +1,17 @@
 const fs = require("fs-extra");
 
-const { CONFIG_FILE, TEMPLATES_DIR } = require("./constants");
+const { CONFIG_FILE, TINPIG_DIR } = require("./constants");
 
 const DEFAULT_CONFIG = {
+  templatesDir: `${TINPIG_DIR}/templates`,
 };
 
 class Configurator {
   configure() {
-    return fs.ensureDir(TEMPLATES_DIR)
-      .then(() => this.getConfig());
+    this.getConfig()
+      .then(config => this.setConfig(config))
+      .then(() => fs.ensureDir(this.config.templatesDir))
+      .then(() => this.config);
   }
 
   getConfig() {
--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,6 @@ const TINPIG_DIR = `${process.env.HOME}/.config/tinpig`;
 const constants = {
   TINPIG_DIR,
   CONFIG_FILE: `${TINPIG_DIR}/config`,
-  TEMPLATES_DIR: `${TINPIG_DIR}/templates`,
   SAMPLE_PROJECTS: `${__dirname}/../templates`,
 };
 
